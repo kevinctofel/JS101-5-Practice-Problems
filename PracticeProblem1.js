b613cf11af5e1381b9b112441619b4a9dafa7c49
Kevin C. Tofel <kevin@kctofel.com>
2022-09-27 15:22:08 -0400

    Completed

--- PracticeProblem1.js
+++ PracticeProblem1.js
@@ -2,3 +2,16 @@
 
 let arr = ['10', '11', '9', '7', '8'];
 
+console.log(arr.sort((a, b) => {
+    if (Number(a) < Number(b)) {
+        return 1;
+    } else if (Number(a) > Number(b)) {
+        return -1;
+    } else {
+        return 0;
+    }
+}));
+
+// refactored
+
+console.log(arr.sort((a, b) => Number (b) - Number(a)));
