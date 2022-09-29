87a76bbef6f1ddea4a6bde8bbff6bd92b41271d1
Kevin C. Tofel <kevinctofel@users.noreply.github.com>
2022-09-29 13:37:08 -0400

    Completed

---
+++ PracticeProblems6a.js
@@ -0,0 +1,16 @@
+// One of the most frequently used real-world string operations is that of "string substitution," where we take a hard-coded string and modify it with various parameters from our program.
+
+// Given this previously seen family object, print the name, age, and gender of each family member
+
+// Example: (Name) is a (age)-year-old (male or female).
+
+let munsters = {
+  Herman: { age: 32, gender: 'male' },
+  Lily: { age: 30, gender: 'female' },
+  Grandpa: { age: 402, gender: 'male' },
+  Eddie: { age: 10, gender: 'male' },
+  Marilyn: { age: 23, gender: 'female'}
+};
+for (let person in munsters) {
+  console.log(`${person} is a ${munsters[person].age}-year-old ${munsters[person].gender}.`);
+}
