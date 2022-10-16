73b5a50cc3a1c68270243b60db94f0866e0e8b1a
Kevin C. Tofel <kevin@kctofel.com>
2022-10-16 19:01:19 -0400

    Initial attempt

---
+++ PracticeProblem13.js
@@ -0,0 +1,22 @@
+// Given the following data structure, sort the array so that the sub-arrays are ordered based on the sum of the odd numbers that they contain.
+
+// Expected result: [ [ 1, 8, 3 ], [ 1, 6, 7 ], [ 1, 5, 3 ] ]
+
+let arr = [[1, 6, 7], [1, 5, 3], [1, 8, 3]];
+
+const sumOfOdds = (arr) => {
+
+  arr.forEach(subArray => {
+    let total = 0;
+    subArray.forEach(element => {
+
+      if (element % 2 !== 0) {
+        total += element;
+      }
+
+    })
+    console.log(total);
+  })
+}
+
+sumOfOdds(arr);
